d495387c124e0d20206821791ada71a914ac301d
tarumoto-sonosuke <sonosuke.tarumoto@uzabase.com>
2024-04-20 01:20:20 +0900

    jest setting wip

---
+++ __test__/Home.test.tsx
@@ -0,0 +1,10 @@
+// import { render, screen } from "@testing-library/react";
+// import React from "react";
+// import Home from "@/app/page";
+
+// describe("Home", () => {
+//   it("should render Home", async () => {
+//     render(<Home />);
+//     expect(await screen.findByText("ランダム都市：")).toBeInTheDocument();
+//   });
+// });
