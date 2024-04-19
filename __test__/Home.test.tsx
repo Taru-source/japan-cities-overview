0daca55759ba112af1c709281ca621df1c60ea42
tarumoto-sonosuke <sonosuke.tarumoto@uzabase.com>
2024-04-20 01:27:43 +0900

    .test.envを読んでくれるようになったぞ

--- __test__/Home.test.tsx
+++ __test__/Home.test.tsx
@@ -1,14 +1,10 @@
-// import { render, screen } from "@testing-library/react";
-// import React from "react";
-// import Home from "@/app/page";
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import Home from "@/app/page";
 
-// describe("Home", () => {
-//   it("should render Home", async () => {
-//     render(<Home />);
-//     expect(await screen.findByText("ランダム都市：")).toBeInTheDocument();
-//   });
-// });
-
-test("adds 1 + 2 to equal 3", () => {
-  expect(1 + 2).toBe(3);
+describe("Home", () => {
+  it("should render Home", async () => {
+    render(<Home />);
+    expect(await screen.findByText("ランダム都市：")).toBeInTheDocument();
+  });
 });
