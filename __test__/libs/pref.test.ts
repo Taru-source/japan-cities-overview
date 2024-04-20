deaad4867a21152979f5c1cdc88a5ee5150b0743
tarumoto-sonosuke <sonosuke.tarumoto@uzabase.com>
2024-04-20 20:02:52 +0900

    add tests for pref.ts

---
+++ __test__/libs/pref.test.ts
@@ -0,0 +1,27 @@
+import { city } from "@/libs/city";
+import { Endpoint } from "@/libs/client";
+import { avgPrefRent, fetchCities, prefName } from "@/libs/pref";
+import { createClient } from "microcms-js-sdk";
+
+describe("prefName", () => {
+  it("should return prefName by prefCode", () => {
+    const prefCode = 1;
+    expect(prefName(prefCode)).toBe("北海道");
+  });
+
+  it("should return N/A if prefCode is not found", () => {
+    const prefCode = 100;
+    expect(prefName(prefCode)).toBe("N/A");
+  });
+});
+
+describe("avgPrefRent", () => {
+  it("渡されたcitiesのrentの平均値を返す", () => {
+    const cities = [
+      { rentPerSqm: 1000 },
+      { rentPerSqm: 2000 },
+      { rentPerSqm: 3000 },
+    ] as city[];
+    expect(avgPrefRent(cities)).toBe(2000);
+  });
+});
