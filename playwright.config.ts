e5a9507bf371b9200edcae749945e5978f178a1f
tarumoto-sonosuke <sonosuke.tarumoto@uzabase.com>
2024-06-22 21:26:39 +0900

    concurrently要らないらしい

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,4 +5,8 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: "http://localhost:3000",
   },
+  webServer: {
+    command: "npm run dev",
+    port: 3000,
+  },
 });
